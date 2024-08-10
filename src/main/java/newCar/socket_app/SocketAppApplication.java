4f76f79c535ef60bbe70e99e724a71c3676766ed
장준하 <jangjunha@jangjunhaui-MacBookPro.local>
2024-08-10 13:37:55 +0900

    프로젝트 환경 설정

---
+++ src/main/java/newCar/socket_app/SocketAppApplication.java
@@ -0,0 +1,13 @@
+package newCar.socket_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/newCar/socket_app/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package newCar.socket_app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override //STOMP 엔드포인트 설정
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+    @Override //메시지를 라우팅
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+}
+
