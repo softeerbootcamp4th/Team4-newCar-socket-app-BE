a88be44651f24f98d6f1697dd161880e9d3e71fa
junha <yy4124@gmail.com>
2024-08-10 18:41:30 +0900

    MessageBrokerService의 sendMessage()는 어느 토픽으로 publish할지 지정해야한다

--- src/main/java/newCar/socket_app/service/MessageBrokerService.java
+++ src/main/java/newCar/socket_app/service/MessageBrokerService.java
@@ -1,2 +1,7 @@
-package newCar.socket_app.service;public interface MessageBrokerService {
+package newCar.socket_app.service;
+
+import newCar.socket_app.model.ChatMessage;
+
+public interface MessageBrokerService {
+    public void sendMessage(String topic, ChatMessage message);
 }
