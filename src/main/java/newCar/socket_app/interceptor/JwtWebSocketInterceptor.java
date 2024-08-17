4b4559ccaee73c3912a40fb4862bf6d75ca12f22
junha <yy4124@gmail.com>
2024-08-18 02:59:35 +0900

    t

--- src/main/java/newCar/socket_app/interceptor/JwtWebSocketInterceptor.java
+++ src/main/java/newCar/socket_app/interceptor/JwtWebSocketInterceptor.java
@@ -28,6 +28,8 @@ public class JwtWebSocketInterceptor implements HandshakeInterceptor {
             WebSocketHandler wsHandler,
             Map<String, Object> attributes
     ) throws Exception {
+        log.info("웹소켓 연결요청 HTTP로 들어옴!!!!!!!!");
+        log.info(request.toString());
         String token = request.getHeaders().getFirst("Authorization");
 
         if (token != null) {
