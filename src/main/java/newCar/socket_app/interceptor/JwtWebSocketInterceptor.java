bdf4e2ff2b4ee4e9f674127ee1700fd9cc47a040
junha <yy4124@gmail.com>
2024-08-17 14:40:11 +0900

    인터셉터 토큰 검증 및 역할에 따른 세션 분리

--- src/main/java/newCar/socket_app/interceptor/JwtWebSocketInterceptor.java
+++ src/main/java/newCar/socket_app/interceptor/JwtWebSocketInterceptor.java
@@ -1,10 +1,10 @@
 package newCar.socket_app.interceptor;
 
 import lombok.RequiredArgsConstructor;
-import newCar.socket_app.model.SocketSession;
+import newCar.socket_app.model.session.AdminSession;
+import newCar.socket_app.model.session.UserSession;
 import newCar.socket_app.model.Team;
 import newCar.socket_app.service.secure.JwtTokenProvider;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -28,14 +28,17 @@ public class JwtWebSocketInterceptor implements HandshakeInterceptor {
     ) throws Exception {
         String token = request.getHeaders().getFirst("Authorization");
 
+        // WebSocket 연결 시 클라이언트를 식별할 수 있는 고유한 세션을 생성한다. -> 웹소켓 연결 자체가 고유한 세션 식별자로 묶여있다.
+        // 클라이언트가 소켓을 통해 보내는 메시지에 세션 식별자를 '명시적으로' 작성하지 않아도 서버는 클라이언트를 식별할 수 있다.
         if (token != null && jwtTokenProvider.validateToken(token)) {
-            // WebSocket 연결 시 클라이언트를 식별할 수 있는 고유한 세션을 생성한다. -> 웹소켓 연결 자체가 고유한 세션 식별자로 묶여있다.
-            // 클라이언트가 소켓을 통해 보내는 메시지에 세션 식별자를 '명시적으로' 작성하지 않아도 서버는 클라이언트를 식별할 수 있다.
+            if(jwtTokenProvider.validateAdminToken(token)){
+                attributes.put("session", new AdminSession());
+                return true;
+            }
+
             Long userId = jwtTokenProvider.getUserId(token);
             Team team = jwtTokenProvider.getTeam(token);
-
-            // 세션에 저장될 속성 초기화. -> 서버 내부에 저장됨.
-            attributes.put("session", new SocketSession(userId, team));
+            attributes.put("session", new UserSession(userId, team));
         }
 
         return true;
