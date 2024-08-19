54ee1f14300df8d24f8596c73ced29546baaa554
junha <yy4124@gmail.com>
2024-08-20 01:13:30 +0900

    사용되지 않는 클래스 삭제

--- src/main/java/newCar/socket_app/model/chat/BlockMessage.java
+++ src/main/java/newCar/socket_app/model/chat/BlockMessage.java
@@ -8,8 +8,7 @@ import lombok.Setter;
 
 import java.io.Serializable;
 
-@Getter
-@Setter
+@Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
--- src/main/java/newCar/socket_app/model/chat/UserChatMessage.java
+++
@@ -1,5 +0,0 @@
-package newCar.socket_app.model.chat;
-
-public class UserChatMessage extends ChatMessage {
-
-}
