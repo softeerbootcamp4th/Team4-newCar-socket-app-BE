25a900fd6f6f510149d15560cbcdb043166c669a
junha <yy4124@gmail.com>
2024-08-19 11:20:39 +0900

    HealthCheck Controller 추가

---
+++ src/main/java/newCar/socket_app/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package newCar.socket_app.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
+    }
+}
