e4f1d92527e7d96ff03d9bee66ff5f471fba1c14
junha <yy4124@gmail.com>
2024-08-19 11:23:12 +0900

    health Check 경로 / -> /health

--- src/main/java/newCar/socket_app/controller/HealthCheckController.java
+++ src/main/java/newCar/socket_app/controller/HealthCheckController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
-    @GetMapping("/")
+    @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("OK");
     }
