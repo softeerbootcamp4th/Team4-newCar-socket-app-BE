a6ad8fd9fbf5872c63b5c048abeaa249dc3d8bd3
junha <yy4124@gmail.com>
2024-08-19 14:25:52 +0900

    health check

--- src/main/java/newCar/socket_app/controller/HealthCheckController.java
+++ src/main/java/newCar/socket_app/controller/HealthCheckController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
     @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
-        return ResponseEntity.ok("OK");
+        return ResponseEntity.ok("Health Check OK");
     }
 }
