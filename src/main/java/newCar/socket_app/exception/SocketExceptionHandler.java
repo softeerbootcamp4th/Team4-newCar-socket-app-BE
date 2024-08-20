92547d626b5361cb0f5fc404490e4dc805863c1d
junha <yy4124@gmail.com>
2024-08-20 15:55:01 +0900

    컨트롤러에서 발생하는 예외 /queue/errors 로 전송, 클라이언트는 /user/queue/errors 로 구독

--- src/main/java/newCar/socket_app/exception/SocketExceptionHandler.java
+++ src/main/java/newCar/socket_app/exception/SocketExceptionHandler.java
@@ -4,7 +4,7 @@ import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
-//@ControllerAdvice
+@ControllerAdvice
 public class SocketExceptionHandler {
 
     @MessageExceptionHandler(RuntimeException.class)
